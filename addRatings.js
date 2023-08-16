d35c5c728c0a15c8a347b78b1535b6d7a12ad9fe
ibzimh <ibzimh@gmail.com>
2023-08-16 17:03:51 +0500

    removing extra comments

--- addRatings.js
+++ addRatings.js
@@ -8,7 +8,7 @@ addRatings();
 function getLinks() {
   const links = document.querySelectorAll('[href*="mailto:"]');
 
-  // retry in XXX seconds if the links are not found
+  // retry in 5 seconds if the links are not found
   return links.length > 0 ? Promise.resolve(links) : new Promise((resolve, _) => {
       setTimeout(() => resolve(getLinks()), 5000);
   });
@@ -24,24 +24,24 @@ function addRatings() {
     })
     .then(data_and_links => {
       let [data, links] = data_and_links;
-      // iterate through all links
+
       for (const link of links) {
         // (1) NameOfTheProfessor <span style="color:theColor">theRating</span>
         let current_link = link.innerHTML.toString();
-        // Put all the 'phrases' in (1) into an array and remove ones which are only spaces
+        // put all the text of (1) into an array and remove ones which are only spaces
         let names_array = current_link.trim().split(" ").filter(entry => entry.trim() !== "");
 
         let first_name = names_array[0];
         let last_name = names_array[1];
 
-        // get the ratings for the current professor
+        // ratings for the current professor
         function getProfRating() {
-          // get the professor from the data (by their name)
+          // search for the professors 
           let professor = data.filter(prof => prof.tFname === first_name && prof.tLname === last_name)
           
-          // if no professors with the name have been found then do a more lenient search 
+          // if no professors found then do a more lenient search 
           if (professor.length === 0) {
-            // check if a shortened version of the name is displayed (or if last and middle names have been mixed up)
+            // if a shortened version of the name is displayed (or if last and middle names have been mixed up)
             professor = data.filter(prof => {
               return (prof.tFname.indexOf(first_name) === 0) &&
               (prof.tMiddlename.indexOf(last_name) === 0 || prof.tLname.indexOf(last_name) === 0)
@@ -50,11 +50,10 @@ function addRatings() {
           
           return professor.length > 0 ? professor[0].overall_rating : undefined;
         }
-
-        // call the function ^ 
+ 
         let prof_rating = getProfRating();
 
-        // If there isn't any rating for the professor 
+        // If no rating found
         if (prof_rating === undefined) {
           prof_rating = "No rating found!";
         }
