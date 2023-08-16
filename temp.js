b3bc7bc3f7142d53bc071ea66752270240bff695
ibzimh <ibzimh@gmail.com>
2023-08-16 18:00:50 +0500

    Create temp.js

---
+++ temp.js
@@ -0,0 +1,79 @@
+const func1 = () => {
+    console.log("1. i've been called");
+    addRatings();
+}
+
+const func2 = () => {
+    console.log("2. i've been called");
+    setTimeout(func1, 10000);
+}
+
+document.getElementsByClassName("PSPUSHBUTTON")[2].addEventListener("click", () => console.log("1. button clicked"));
+document.getElementsByClassName("PSPUSHBUTTON")[3].addEventListener("click", () => console.log("2. button clicked"));
+document.getElementsByClassName("PSPUSHBUTTON")[6].addEventListener("click", () => console.log("3. button clicked"));
+document.getElementsByClassName("PSPUSHBUTTON")[7].addEventListener("click", () => console.log("4. button clicked"));
+
+document.getElementsByClassName("PSPUSHBUTTON")[2].addEventListener("click", func2);
+document.getElementsByClassName("PSPUSHBUTTON")[3].addEventListener("click", func2);
+document.getElementsByClassName("PSPUSHBUTTON")[6].addEventListener("click", func2);
+document.getElementsByClassName("PSPUSHBUTTON")[7].addEventListener("click", func2);
+
+
+function addRatings() {
+    // gets the name of the professors
+    let links = document.querySelectorAll('[href*="mailto:"]');
+    console.log(links);
+    // the json with the professor's ratings
+    let url =
+      "https://raw.githubusercontent.com/ibzimh/UMA_RMP_Data/main/professorsJSON.json";
+  
+    fetch(url)
+      .then((response) => response.json())
+      .then(data => {
+        // iterate through all links
+        for (const link of links) {
+          // (1) NameOfTheProfessor <span style="color:theColor">theRating</span>
+          let current_link = link.innerHTML.toString();
+          // Put all the 'phrases' in (1) into an array and remove ones which are only spaces
+          let names_array = current_link.trim().split(" ").filter(entry => entry.trim() !== "");
+  
+          let first_name = names_array[0];
+          let last_name = names_array[1];
+  
+          // get the ratings for the current professor
+          function getProfRating() {
+            // get the professor from the data (by their name)
+            let professor = data.filter(prof => prof.tFname === first_name && prof.tLname === last_name)
+            
+            // if no professors with the name have been found then do a more lenient search 
+            if (professor.length === 0) {
+              // check if a shortened version of the name is displayed (or if last and middle names have been mixed up)
+              professor = data.filter(prof => {
+                return (prof.tFname.indexOf(first_name) === 0) &&
+                (prof.tMiddlename.indexOf(last_name) === 0 || prof.tLname.indexOf(last_name) === 0)
+              })
+            }
+            
+            return professor.length > 0 ? professor[0].overall_rating : undefined;
+          }
+  
+          // call the function ^ 
+          let prof_rating = getProfRating();
+  
+          // If there isn't any rating for the professor 
+          if (prof_rating === undefined) {
+            prof_rating = "No rating found!";
+          }
+  
+          // alas! display the ratings
+          if (prof_rating >= 4) {
+            link.innerHTML += ` <span style="color:green">${prof_rating}</span>`;
+          } else if (prof_rating >= 3) {
+            link.innerHTML += ` <span style="color:orange">${prof_rating}</span>`;
+          } else {
+            link.innerHTML += ` <span style="color:red">${prof_rating}</span>`;
+          }
+        }
+      });
+  }
+  
