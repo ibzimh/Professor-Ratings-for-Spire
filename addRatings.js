bff96ded9ca825cfe1ad8c981c0c66e2c4bbeebf
ibzimh <ibzimh@gmail.com>
2023-08-16 17:12:45 +0500

    cleaning up some code
    moving getProfRating outside the addRatings function

--- addRatings.js
+++ addRatings.js
@@ -14,6 +14,23 @@ function getLinks() {
   });
 }
 
+// ratings for the current professor
+function getProfRating(data, first_name, last_name) {
+  // search for the professors 
+  let professor = data.filter(prof => prof.tFname === first_name && prof.tLname === last_name)
+  
+  // if no professors found then do a more lenient search 
+  if (professor.length === 0) {
+    // if a shortened version of the name is displayed (or if last and middle names have been mixed up)
+    professor = data.filter(prof => {
+      return (prof.tFname.indexOf(first_name) === 0) &&
+      (prof.tMiddlename.indexOf(last_name) === 0 || prof.tLname.indexOf(last_name) === 0)
+    })
+  }
+  
+  return professor.length > 0 ? professor[0].overall_rating : undefined;
+}
+
 function addRatings() {
   let url = "https://raw.githubusercontent.com/ibzimh/UMA_RMP_Data/main/professorsJSON.json";
   
@@ -31,27 +48,8 @@ function addRatings() {
         // put all the text of (1) into an array and remove ones which are only spaces
         let names_array = current_link.trim().split(" ").filter(entry => entry.trim() !== "");
 
-        let first_name = names_array[0];
-        let last_name = names_array[1];
-
-        // ratings for the current professor
-        function getProfRating() {
-          // search for the professors 
-          let professor = data.filter(prof => prof.tFname === first_name && prof.tLname === last_name)
-          
-          // if no professors found then do a more lenient search 
-          if (professor.length === 0) {
-            // if a shortened version of the name is displayed (or if last and middle names have been mixed up)
-            professor = data.filter(prof => {
-              return (prof.tFname.indexOf(first_name) === 0) &&
-              (prof.tMiddlename.indexOf(last_name) === 0 || prof.tLname.indexOf(last_name) === 0)
-            })
-          }
-          
-          return professor.length > 0 ? professor[0].overall_rating : undefined;
-        }
- 
-        let prof_rating = getProfRating();
+        // data, first name, last name
+        let prof_rating = getProfRating(data, names_array[0], names_array[1]);
 
         // If no rating found
         if (prof_rating === undefined) {
